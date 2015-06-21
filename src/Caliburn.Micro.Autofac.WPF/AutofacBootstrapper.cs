6228c125d626b8aed1e35cb3900729409ae8417c
Giovanni Crescencio Ahuactzi <win-g@hotmail.com>
2015-06-20 22:53:42 -0500

    Update AutofacBootstrapper.cs
    Cnange in definition of contructor for compatibility with Caliburn.Micro 2.0.2

--- src/Caliburn.Micro.Autofac.WPF/AutofacBootstrapper.cs
+++ src/Caliburn.Micro.Autofac.WPF/AutofacBootstrapper.cs
@@ -11,8 +11,14 @@ namespace Caliburn.Micro.Autofac
   /// A strongly-typed version of Caliburn.Micro.Bootstrapper that specifies the type of root model to create for the application.
   /// </summary>
   /// <typeparam name="TRootViewModel">The type of root view model for the application.</typeparam>
-  public class AutofacBootstrapper<TRootViewModel> : Bootstrapper<TRootViewModel>
+  public class AutofacBootstrapper<TRootViewModel> :  BootstrapperBase
   {
+    public AutofacBootstrapper(bool useApplitacion = true) :base(useApplitacion)
+    {
+      Initialize();
+      this.DisplayRootViewFor(typeof(TRootViewModel));                       
+    }
+        
     #region Properties
     protected IContainer Container { get; private set; }
     /// <summary>
