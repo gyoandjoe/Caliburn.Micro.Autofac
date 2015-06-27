27b53aee7dc2c5c03becabb73772a660728d14a5
gyoandjoe <win-g@hotmail.com>
2015-06-27 16:53:29 -0500

    -- Update to Caliburn.Micro 2.0.2.0 -- Update to Autofac 3.5.2 -- Add project Sample SampleWPFNoLibReference (No need reference to Caliburn.Micro.Autofac)

---
+++ samples/Sample-WPF-NoLibReference/AppBootstrapper .cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using SampleWPFNoLibReference.DI;
+
+namespace SampleWPFNoLibReference
+{
+    public class AppBootstrapper : AutofacBootstrapper<ShellViewModel>
+    {
+        protected override void ConfigureBootstrapper()
+        {
+            
+            //  you must call the base version first!
+            
+            base.ConfigureBootstrapper();
+
+            //  override namespace naming convention
+            EnforceNamespaceConvention = false;
+            //  change our view model base type
+            ViewModelBaseType = typeof(IShell);                       
+        }
+    }
+}
