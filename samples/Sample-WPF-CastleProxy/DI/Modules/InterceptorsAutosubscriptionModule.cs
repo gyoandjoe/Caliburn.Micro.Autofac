d4023389f8be16e2331e3297768caaade958d2e9
gyoandjoe <win-g@hotmail.com>
2015-07-04 19:23:38 -0500

    Added Example of Castle Proxy with Caliburn Micro  and Autofac

---
+++ samples/Sample-WPF-CastleProxy/DI/Modules/InterceptorsAutosubscriptionModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Castle.DynamicProxy;
+using Sample_WPF_CastleProxy.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_WPF_CastleProxy.DI.Modules
+{
+    public class InterceptorsAutosubscriptionModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<LoginInterceptor>().InstancePerLifetimeScope().InstancePerDependency();
+        }
+    }
+}
---
+++ samples/Sample-WPF-CastleProxy/Interceptors/LoginInterceptor.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_WPF_CastleProxy.Interceptors
+{
+    public class LoginInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            var x = invocation.Method.Name;
+            
+        }
+    }
+}
